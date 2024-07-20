4fcede70b3916d4fb219b73644bc5d37d0b9fcd6
ringfirex <ringfirex@gmail.com>
2024-07-20 11:47:04 +0900

    프로그램 시작시 테스트데이터10개

--- src/main/java/com/ll/domain/App.java
+++ src/main/java/com/ll/domain/App.java
@@ -1,21 +1,35 @@
-package com.ll;
+package com.ll.domain;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    Scanner scanner;
-    int lastQuotationId;
-    List<Quotation> quotations;
+    private Scanner scanner;
+    private int lastQuotationId;
+    private List<Quotation> quotations;
 
-    App() {
+    public App() {
         scanner = new Scanner(System.in);
         lastQuotationId = 0;
         quotations = new ArrayList<>();
+
+        initTestData();
     }
 
-    void run() {
+    void initTestData() {
+        for (int i = 0; i < 10; i++) {
+            lastQuotationId++;
+            int id = lastQuotationId;
+            String content = "명언" + id;
+            String authorName = "작가" + id;
+
+            Quotation quotation = new Quotation(id, content, authorName);
+            quotations.add(quotation);
+        }
+    }
+
+    public void run() {
         System.out.println("== 명언 앱 ==");
 
         while (true) {
@@ -44,7 +58,7 @@ public class App {
         }
     }
 
-    void actionWrite() {
+    private void actionWrite() {
         System.out.print("명언 : ");
         String content = scanner.nextLine();
 
@@ -60,7 +74,7 @@ public class App {
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
     }
 
-    void actionList() {
+    private void actionList() {
         System.out.println("번호 / 작가 / 명언");
 
         System.out.println("----------------------");
@@ -74,56 +88,46 @@ public class App {
         }
     }
 
-    void actionRemove(Rq rq) {
+    private void actionRemove(Rq rq) {
         int id = rq.getParamAsInt("id", 0);
 
         if (id == 0) {
             System.out.println("id를 정확히 입력해주세요.");
             return; // 함수를 끝낸다.
         }
-        int index = getIndexIfQuotationById(id);
+
+        int index = findQuotationIndexById(id);
+
         if (index == -1) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
             return;
         }
+
         quotations.remove(index);
-        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+
+        System.out.printf("%d번 명언을 삭제되었습니다.\n", id);
     }
 
-    int getIndexIfQuotationById(int id) {
+    private int findQuotationIndexById(int id) {
         for (int i = 0; i < quotations.size(); i++) {
             Quotation quotation = quotations.get(i);
+
             if (quotation.id == id) {
                 return i;
             }
         }
+
         return -1;
     }
 
-    void actionModify(Rq rq) {
+    private void actionModify(Rq rq) {
         int id = rq.getParamAsInt("id", 0);
 
         if (id == 0) {
             System.out.println("id를 정확히 입력해주세요.");
             return; // 함수를 끝낸다.
         }
-        int index = getIndexIfQuotationById(id);
-        if (index == -1) {
-            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
-            return;
-        }
-        Quotation quotation = quotations.get(index);
-        System.out.printf("명언(기존) : %s\n",quotation.content);
-        System.out.println("명언 : ");
-        String content = scanner.nextLine();
 
-        System.out.printf("작가(기존) : %s\n",quotation.authorName);
-        System.out.println("작가 : ");
-        String authorName = scanner.nextLine();
-
-        quotation.content = content;
-        quotation.authorName = authorName;
-
-        System.out.printf("%d번 명언을 수정되었습니다.\n", id);
+        System.out.printf("%d번 명언을 수정합니다.\n", id);
     }
 }
--- src/main/java/com/ll/domain/Rq.java
+++ src/main/java/com/ll/domain/Rq.java
@@ -1,16 +1,17 @@
-package com.ll;
+package com.ll.domain;
+
+import com.ll.standard.util.Ut;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Rq {
-    String cmd;
-    String action;
-    String queryString;
-    Map<String, String> paramsMap;
+    private String cmd;
+    private String action;
+    private String queryString;
+    private Map<String, String> paramsMap;
 
-
-    Rq(String cmd) {
+    public Rq(String cmd) {
         paramsMap = new HashMap<>();
 
         this.cmd = cmd;
@@ -18,7 +19,7 @@ public class Rq {
         String[] cmdBits = cmd.split("\\?", 2);
         action = cmdBits[0].trim();
 
-        if (cmdBits.length == 1) { // ? 없어서 cmdBits에 배열에 요소가 하나뿐인 경우
+        if (cmdBits.length == 1) {
             return;
         }
 
@@ -37,20 +38,11 @@ public class Rq {
         }
     }
 
-    String getAction() {
+    public String getAction() {
         return action;
     }
 
     public int getParamAsInt(String paramName, int defaultValue) {
-        String paramValue = paramsMap.get(paramName);
-
-        if (paramValue != null) {
-            try {
-                return Integer.parseInt(paramValue);
-            } catch (NumberFormatException e) {
-
-            }
-        }
-        return defaultValue;
+        return Ut.str.parseInt(paramsMap.get(paramName), defaultValue);
     }
 }
